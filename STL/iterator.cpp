e9b38bed804b8d8cb1ef723a623bd7df212550fb
syedshazli <syed.abdu1818@gmail.com>
2025-01-28 00:35:21 -0500

    Finish iterator

---
+++ STL/iterator.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <iterator>
+using namespace std;
+int main(int argc, char* argv[]){
+
+    cout<<"Enter two integers"<<endl;
+
+    istream_iterator <int> inputInt(cin); // read int values from cin
+    int num1 = *inputInt; // read int
+    ++inputInt; // move iterator to next input
+    int num2 = *inputInt; // read new int
+
+    ostream_iterator <int> outputInt (cout);
+
+    cout<<"Your sum is ";
+    *outputInt = num1+num2; // output result to cout
+
+    cout<<endl;
+
+}
