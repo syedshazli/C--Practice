8f7d7460b0ce30366591444af69461fd97388148
syedshazli <syed.abdu1818@gmail.com>
2025-03-05 00:03:53 -0500

    finish vec example

--- References/passByRef.cpp
+++ References/passByRef.cpp
@@ -5,6 +5,8 @@
 using namespace std;
 
 // pass by reference
+// remove the & from sameNum to see what no pass by reference does
+// essentially, no variables you edit get changed
 void myRefFunction(int myNum, int &sameNum){
 
     int newNum = myNum + sameNum;
@@ -15,6 +17,12 @@ void myRefFunction(int myNum, int &sameNum){
     cout<<"Incremented B to be "<<sameNum<<" in the myRefFunction"<<endl;
 }
 
+void appendElements(vector<int> &vec, int N){
+    for(int i = 0; i< N; i++){
+        vec.push_back(i);
+    }
+}
+
 int main(){
     int a = 5;
     int &b = a;
@@ -22,6 +30,14 @@ int main(){
 
     cout<<"After function, b == "<<b<<endl;
     cout<<"And a == "<<a<<endl;
+
+    vector<int> myVec;
+    appendElements(myVec, 10);
+    // expected result w/no pbr : we dont see anything bc myVec was passed by value
+    for(auto i: myVec){
+        cout<<i<<" ";
+    }
+    cout<<endl;
 
     return 0;
 }
