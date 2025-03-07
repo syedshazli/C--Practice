56b2e0d80d2235f33274f9b536b0cfad8ec2d148
syedshazli <syed.abdu1818@gmail.com>
2025-03-07 16:30:46 -0500

    finished signed integers

---
+++ Number_Representation/signed.cpp
@@ -0,0 +1,34 @@
+// signed integers
+// lsb is at 2^0
+// msb is 2^32-1, multiplied by -1
+#include <iostream>
+#include <cstdint>
+#include <bitset>
+using namespace std;
+
+int main(){
+    // set signed integer value (32 bits)
+    int32_t value = 1;
+    bitset<32> bits(value);
+
+    // result to calculate
+    int32_t result = 0;
+
+    // iterate thru all 32 bits
+    for(int i = 0; i<32; i++){
+        if(bits[i]){
+            // current but is 1
+            result += 1 <<i;
+            // bit 31 is active, negative integer
+            if(i == 31){
+                cout<< "-2^"<<i<<" = "<<(1<<i)<<endl;
+            }
+            else{
+                // bit 32 is low, positive integer
+                cout<< "2^"<<i<<" = "<<(1<<i)<<endl;
+            }
+        }
+    }
+
+
+}
