35314fbb4decbfd22096958221eadd8eb06bf73e
syedshazli <syed.abdu1818@gmail.com>
2025-03-18 15:18:03 -0400

    create smart pointers using objects

---
+++ Pointers/smartObjectPtrs.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+using namespace std;
+
+struct myBooks{
+    int numBooks;
+    string favBook;
+    double totalPriceOfBooks;
+};
+
+int main(){
+
+}
