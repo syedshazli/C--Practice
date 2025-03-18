52cde0c28539bf6f55d518422d1ddd62a9257a7c
syedshazli <syed.abdu1818@gmail.com>
2025-03-18 18:29:27 -0400

    Finished unique pointers, edit object thru shared ptr in sharedptr file

--- Pointers/Smart_Pointers/objectUniquePtr.cpp
+++ Pointers/Smart_Pointers/objectUniquePtr.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <memory>
+#include <string>
+using namespace std;
 
-void uniquePtrFunc(){
-    //unique_ptr<A> ptr1 (new A)
+struct myClass{
+    int numStudents;
+    string teacher;
+    bool isClean;
+};
+void uniquePtrFunc(unique_ptr<myClass> &myClassroom){
+    cout<<"Hello from the function!"
+    myClassroom->isClean = true;
+   
 }
 
 int main(){
+    //unique_ptr<A> ptr1 (new A)
+    unique_ptr<myClass> classOne(new myClass);
+    classOne->numStudents = 5;
+
+    int findNumStuds = classOne->numStudents;
+    cout<<"Number of students: "<<findNumStuds<<endl;
+
+    uniquePtrFunc(classOne);
+    cout<<endl;
+    cout<<"I just changed a value of the pointer thru a function. Let's see if it worked"<<endl;
+    cout<<classOne->isClean<<endl;
 
 }
