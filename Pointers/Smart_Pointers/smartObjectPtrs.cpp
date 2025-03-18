4dd8e7356586d3e59e1861e48f8d811c9f9e294c
syedshazli <syed.abdu1818@gmail.com>
2025-03-18 17:48:30 -0400

    Added modifications for other values

--- Pointers/Smart_Pointers/smartObjectPtrs.cpp
+++ Pointers/Smart_Pointers/smartObjectPtrs.cpp
@@ -11,11 +11,18 @@ struct myBooks{
 };
 
 int main(){
-    myBooks syedsBooks; // create object
+  
 
     // using a shared pointer because when using unique pointer, we expectedley run into issues
-    shared_ptr<myBooks> syedsPointer(new myBooks); 
+    shared_ptr<myBooks> syedsPointer(new myBooks); // create pointer (dynamically allocates memory for a myBook object, creates a pointer that points to it)
     syedsPointer->numBooks = 5;
-    cout<<"number of books for pointer: "<<syedsPointer->numBooks<<endl;
+    syedsPointer->favBook = "Computer Architecture: A Quantitative Approach";
+    syedsPointer->totalPriceOfBooks = 650.70;
+
+    double syedsPrice = syedsPointer->totalPriceOfBooks; // example setting a variable to a value that a smart pointer is pointing to
+
+    cout<<"Number of books for syedspointer: "<<syedsPointer->numBooks<<endl;
+    cout<<"Syed's Pointer's favorite book: "<<syedsPointer->favBook<<endl;
+    cout<<"Total Price of Syed's pointer books: "<<syedsPrice<<endl;
     
 }
