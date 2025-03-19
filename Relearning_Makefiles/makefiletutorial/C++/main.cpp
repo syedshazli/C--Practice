05087f3752696c01d6cf877b1c0847bc920b042e
syedshazli <syed.abdu1818@gmail.com>
2025-03-19 17:03:40 -0400

    Start testing multifile implementation

---
+++ Relearning_Makefiles/makefiletutorial/C++/main.cpp
@@ -0,0 +1,11 @@
+#include <refer.hpp>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(){
+    cout<<"Greetings, this is main.cpp, I am going to try and get a function from refer.hpp which refers to refer.cpp. Wish me luck"<<endl;
+    printFromRefer();
+}
---
+++ Relearning_Makefiles/makefiletutorial/C++/refer.cpp
@@ -0,0 +1,12 @@
+#include <refer.hpp>
+#include <iostream>
+
+using namespace std;
+
+void printFromRefer(){
+    cout<<"Hey, this is the refer file speaking. I was probably found through the .h file"<<endl;
+}
+
+int main(){
+    
+}
