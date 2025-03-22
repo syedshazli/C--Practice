3734806b6bab54abf4f79674d82ac435488cc321
Syed Shazli <sashazli@login-01.cm.cluster>
2025-03-22 16:40:44 -0400

    feat: finished getting more stats

--- CUDA/CUDABYEXAMPLE/deviceInfo/deviceInfo.cu
+++ CUDA/CUDABYEXAMPLE/deviceInfo/deviceInfo.cu
@@ -1,4 +1,5 @@
 #include <iostream>
+// find more on page 30 of the book
 
 using namespace std;
 
@@ -12,8 +13,22 @@ int main(void){
 			// let's see what we can find
 
 	// some things that might be interesting
+	cout<<"---General informaiton for device---"<<endl;
 	cout<<"Name of GPU: "<<prop.name<<endl;
+	cout<<"Compute capability: "<<prop.major<<"and " <<prop.minor<<endl;	    cout<<"Clock rate: "<<prop.clockRate<<endl;
 	
+	cout<<"---Memory Information---"<<endl;
+	cout<<"Total global memory: "<<prop.totalGlobalMem<<" bytes"<<endl;
+	cout<<"Total constant memory: "<<prop.totalConstMem<<endl;
+	cout<<"Max mem pitch: "<<prop.memPitch<<endl;
+	
+	cout<<"---Multiprocessor Info for device "<<i<<endl;
+	cout<<"Multiprocessor count: "<<prop.multiProcessorCount<<endl;
+	cout<<"Shared memory per multiprocessor: "<<prop.sharedMemPerBlock<<" bytes"<<endl;
+	cout<<"32 bit Registers per multiprocessor: "<<prop.regsPerBlock<<endl;
+	cout<<"Threads in warp: "<<prop.warpSize<<endl;
+	cout<<"Max threads per block: "<<prop.maxThreadsPerBlock<<endl;
+
 	}//end of for loop
 
 }
