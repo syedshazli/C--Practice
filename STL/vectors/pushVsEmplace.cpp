3b64a60069767bcb0487f7c022ee260991fe8953
syedshazli <syed.abdu1818@gmail.com>
2025-03-31 22:44:06 -0400

    done with psuh vs empalce back

--- STL/vectors/pushVsEmplace.cpp
+++ STL/vectors/pushVsEmplace.cpp
@@ -25,6 +25,21 @@ int main(){
 
 // we want dynamically sized arrays, we must know how to use it well
 vector<myStruct> myVector;
+myStruct newObj(10);
+myVector.push_back(newObj); // copy constructor
 
+// done using move
+myStruct usingMove(2);
+myVector.push_back(move(usingMove));
+
+// done using dynamic alloc of object
+myVector.push_back(myStruct(1));
+
+// there's a cost of constantly doing copies
+// so we use emplace back
+// appends new element to container (vector)
+// constructs element in place at location of container
+// construct object inside memory by vector
+myVector.emplace_back(60);
 
 }
