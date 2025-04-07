c40e15ac0071f39203db366b64dc9fb060c35039
Syed Shazli <sashazli@login-02.cm.cluster>
2025-04-07 12:37:17 -0400

    added multiplication as well

--- CUDA/CUDABYEXAMPLE/threadCooperation/vectorSum.cu
+++ CUDA/CUDABYEXAMPLE/threadCooperation/vectorSum.cu
@@ -2,12 +2,12 @@
 #define N 20
 using namespace std;
 
-__global__ void add(int *a, int *b, int *c){
+__global__ void add(int *a, int *b, int *c, int *d){
 
 	int threadID  = threadIdx.x;
 	if(threadID < N){
 	
-		c[threadID] = a[threadID] + b[threadID];
+		c[threadID] = (a[threadID] + b[threadID]) * d[threadID];
 	
 	}
 
@@ -17,18 +17,19 @@ __global__ void add(int *a, int *b, int *c){
 int main(void){
 	
 
-	int a[N], b[N], c[N];
-	int *device_a, *device_b, *device_c;
-
+	int a[N], b[N], c[N], d[N];
+	int *device_a, *device_b, *device_c, *device_d;
 	//allocate memory on the device
 	cudaMalloc( (void**)&device_a, N*sizeof(int)  );
 	cudaMalloc( (void**)&device_b, N* sizeof(int)  );
 	cudaMalloc( (void**)&device_c, N*sizeof(int) );
+	cudaMalloc( (void**)&device_d, N*sizeof(int) );
 
 	for(int i = 0; i<N; i++){
 	
 		a[i] = i;
 		b[i] = i*i;
+		d[i] = (int)i/2;
 	}
 
 
@@ -36,24 +37,28 @@ int main(void){
 
 	cudaMemcpy(device_a, a, N*sizeof(int), cudaMemcpyHostToDevice );
 	cudaMemcpy(device_b, b, N*sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(device_d, d, N*sizeof(int), cudaMemcpyHostToDevice );
+	
 
 	// compute the addition
 	// 1 thread block, 20 threads on the block
-	add<<<1,N>>>(device_a, device_b, device_c);
+	add<<<1,N>>>(device_a, device_b, device_c, device_d);
 
 	// copy array c from GPU to CPu so we can view results
 	cudaMemcpy(c, device_c, N*sizeof(int), cudaMemcpyDeviceToHost);
 
 	for(int i = 0; i<N; i++){
 	
-		cout<<a[i]<< " + "<<b[i] <<" = "<<c[i]<<endl;
+		cout<<a[i]<< " + "<<b[i] <<" * "<<d[i]<<" = "<<c[i]<<endl;
 	}
 
 
 
 	cudaFree(device_a);
 	cudaFree(device_b);
+	cudaFree(device_d);
 	cudaFree(device_c);
+	
 	return 0;
 	}
 
