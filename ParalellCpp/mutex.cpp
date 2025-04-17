c0f87ffda8a578285efd84f5c7e63e9ff3970d7d
syedshazli <syed.abdu1818@gmail.com>
2025-04-16 23:11:04 -0400

    finish mutex

---
+++ ParalellCpp/mutex.cpp
@@ -0,0 +1,46 @@
+#include <thread>
+#include <iostream>
+#include <mutex>
+#include <vector>
+using namespace std;
+
+mutex myMutex;
+mutex intAccess;
+
+void printTID(int tid, int *intIncrement){
+    // if a thread has locked the mutex alr, we wait till the other thread is done
+    // note that we enter deadlock if we never unlock!
+
+    // RAII --> Get some resource in a constructor, release in the destructor
+    // lock in constructor, unlock in destructor
+    // done using lock_guard
+
+    // if using lock guard, we don't need lock or unlock methods
+    lock_guard<mutex> lg(intAccess);
+    //intIncrement+=1;
+    //cout<<intIncrement<<endl;
+    myMutex.lock();    
+    cout<<"I'm thread #"<<tid+1<<endl;
+    myMutex.unlock();
+}
+
+
+
+int main(){
+    // concurrent access of shared resource
+    // for us it was trying to cout at the same time
+
+    // mutex prevents shared data from being simulatenously accessed (OS)
+    
+    vector<thread> arrayThreads(6);
+    int *incrementInt = 0;
+    for(int i = 0; i<arrayThreads.size(); i++){
+        // orderings of prints may differ, which is ok
+        arrayThreads[i] = thread(printTID, i, &incrementInt); 
+    }
+
+    for(int i = 0; i<arrayThreads.size(); i++){
+        arrayThreads[i].join(); 
+    }
+
+}
--- ParalellCpp/threads.cpp
+++ ParalellCpp/threads.cpp
@@ -1,9 +1,10 @@
 #include <thread>
 #include <iostream>
+#include <string>
 #include <vector>
 using namespace std;
 
-void doSomethingThready(int tid){
+void doSomethingThready(int tid ){
     cout<<"I'm thread #"<<tid<<endl;
 }
 
@@ -14,14 +15,22 @@ int main(){
     // so tell one thread to execute a function, and while that happens, continue with main thread execution
     
     thread myThread1(doSomethingThready, 0);
-    myThread1.join();
+
+    // my main thread running the main function will wait here till myThread1 is done
+    //myThread1.join();
     
-    // make 6 threads
+    // make 6 threads, and have them execute a function
     vector<thread> arrThreads(6);
 
     for(int i = 1; i<6; i++){
         arrThreads[i] = thread(doSomethingThready, i);
     }
 
+    for(int i = 1; i<6; i++){
+        arrThreads[i].join();
+    }
     
+    // C++ 20: Jthread
+    // automatically joins upon destruction
+
 }
