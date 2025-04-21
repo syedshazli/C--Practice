a859181997399f594293c7d7bd007c377dd756f5
syedshazli <syed.abdu1818@gmail.com>
2025-04-21 15:21:12 -0400

    finish random nums

---
+++ Basic_Concepts/randomNumbers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <random>
+
+int main(){
+    // produces Non deterministic random numbers
+
+    std::random_device rd;
+
+    // mersene twister engine
+    std::mt19937 mt(rd());
+    std::uniform_int_distribution uniform(1,10); // nums between 1 - 10
+    for(int i = 0; i<5; i++){
+        std::cout<<uniform(mt); // random number gen w/ uniform dist
+        std::cout<<'\n';
+    }
+    std::cout<<'\n';
+
+    return 0;
+}
