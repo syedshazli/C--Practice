60a504bb7c54ed7d7611a1cc2d034e2a1d7a4cf8
syedshazli <syed.abdu1818@gmail.com>
2025-04-23 22:39:44 -0400

    finish input fstream

--- Basic_Concepts/IO/outputFiles.cpp
+++ Basic_Concepts/IO/outputFiles.cpp
@@ -3,10 +3,10 @@
 
 // ofstream: high level file stream output operationss
 int main(){
-    // store i*i in a file to then view
+    // store i*i in a file to then view, creates a result.txt
     std::ofstream output("result.txt");
     for(int i = 0; i < 10; i++){
-        output << i*i<<' '; 
+        output << i*i<<'\n'; 
     }
-    output <<'\n';
+    // now to reqd data, we use input file stream
 }
