91db2214f17819f28f98b32a16fff938c5f55801
syedshazli <syed.abdu1818@gmail.com>
2025-04-25 12:46:20 -0400

    finish benchmark, make sure it's compatible with make

--- Computer_Arch/pipelining/benchmarkPipeline.cpp
+++ Computer_Arch/pipelining/benchmarkPipeline.cpp
@@ -5,6 +5,7 @@
 #include <queue>
 #include "runningInstructions.hpp"
 
+using namespace std;
 /**
  * benchmarking pipelining:
  * 1. All 6 instructions still in waiting queeu
@@ -26,14 +27,40 @@ int main(){
     waiting.push(i3);
     waiting.push(i4);
     waiting.push(i5);
+    int length = waiting.size();
 
     // create a queue of instructions that are currently running
     deque<Instruction> runningInstruction;
-    int clockCycleLength = 18;
+    int clockCycleLength = 30;
+    // no instructions are done yet
+    int instructionBarrier = 0;
+    // start the clock
+    for(int clock = 0; clock <clockCycleLength; clock++){
 
-    for(int i = 0; i<clockCycleLength; i++){
+        // without pipelining, we don't run instructions at the same time
+        // hence, only 1 instruction can be running at a time
+        if(runningInstruction.size() == 0){
+            
+            runningInstruction.emplace_back(waiting.front());
+            waiting.pop();
+        }
 
-        
+        // for the only instruction that's running, move it to the next stage every clock cycle
+        for(Instruction &currentInstruction : runningInstruction){
+            moveToNextStage(currentInstruction.instructionSequences, clock);
+            instructionBarrier+=1;
+        }
+
+        if(instructionBarrier == length){
+            // current instruction is done running
+            runningInstruction.pop_front();
+
+            cout<<"I finished running all my instructions at clock cycle "<<clock<<endl;
+            // reset instruction barrier
+            instructionBarrier = 0;
+
+        }
+
     }
 
 
