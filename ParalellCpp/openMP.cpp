932cccbd161ce6fabc21deb0a6982f804486a852
syedshazli <syed.abdu1818@gmail.com>
2025-04-27 12:41:23 -0400

    finish pthreads

--- ParalellCpp/openMP.cpp
+++ ParalellCpp/openMP.cpp
@@ -1,4 +1,19 @@
-#include <thread>
+#include <omp.h>
 #include <iostream>
 
-int main(){}
+int main(){
+    // everything in this block will be in paralell 
+#pragma omp parallel{
+    // declare critical region, no conflicts with cout
+#pragma omp critical{
+std::cout << "Printing from thread: "<<omp_get_thread_num() <<'\n';
+}
+
+}
+    
+
+
+  return 0;
+
+
+}
