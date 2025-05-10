497583ecca1103a2498d82323eef6a080ef6fe97
syedshazli <syed.abdu1818@gmail.com>
2025-05-10 17:54:59 -0400

    Make more robust by including vectors. TODO: Use namespaces/templates

--- MatMul/matmul.cpp
+++ MatMul/matmul.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 // pass by reference
-void matmul(int A[4][4], int B[4][4], int C[4][4], int N){
+void matmul(std::vector<std::vector<int>> A, std::vector<std::vector<int>> B, std::vector<std::vector<int>> &C, int N){
     
     for(int row = 0; row < N; row++){
         for(int col = 0; col<N; col++){
@@ -13,7 +13,8 @@ void matmul(int A[4][4], int B[4][4], int C[4][4], int N){
             // col = 3, tryCols = 3.. what happens??? --> This is OOB
 
             for(int tryCols = 0; tryCols < N; tryCols++){
-                
+                // row stays the same and increment by N in first array A
+                // col stays the same and increment by N in second array B
                 value+= A[row][tryCols] * B[tryCols][col]; // iterate thru matmul
 
 
@@ -28,23 +29,29 @@ void matmul(int A[4][4], int B[4][4], int C[4][4], int N){
 int main(){
 
     // declare 2d Arrays A & B
-    int a[4][4] = {
-        1, 1, 1, 1,
-        1, 1, 1, 1,
-        1, 1, 1, 1,
-        1, 1, 1, 1,
-    };
+    std::vector<std::vector<int>> a(4, std::vector<int>(4, 1));
+    // int a[4][4] = {
+    //     1, 1, 1, 1,
+    //     1, 1, 1, 1,
+    //     1, 1, 1, 1,
+    //     1, 1, 1, 1,
+    // };
 
-    int b[4][4] = {
-        2, 2, 2, 2,
-        2, 2, 2, 2,
-        2, 2, 2, 2,
-        2, 2, 2, 2,
-    };
+    // int b[4][4] = {
+    //     2, 2, 2, 2,
+    //     2, 2, 2, 2,
+    //     2, 2, 2, 2,
+    //     2, 2, 2, 2,
+    // };
+
+    // all entries initialised by 2
+    std::vector<std::vector<int>> b(4, std::vector<int>(4, 2));
 
 
-    int c[4][4];
+    // initialise with 0, will be filled in by res of A * B in future
+    std::vector<std::vector<int>> c(4, std::vector<int> (4,0)  );
 
+    // pass by ref in function so C can be modified
     matmul(a,b,c, 4);
 
 
