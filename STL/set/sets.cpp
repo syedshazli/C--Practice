03dff73dfc73e9d4e49471fdd3bf6685b22102d3
syedshazli <syed.abdu1818@gmail.com>
2025-05-26 16:51:18 -0700

    show ascending order

--- STL/set/sets.cpp
+++ STL/set/sets.cpp
@@ -5,11 +5,23 @@ int main(){
     // in sets, the value IS the key.. so no keys can be the same and no vals can be the same
     // ONLY UNIQUE VALUES
     // cant be modified once accessed
-
+    // NOT TO BE CONFUSED WITH A HASHMAP!!! NO <INT, INT> KIND OF RELATIONSHIP
     std::set<int> mySet;
     mySet.emplace(10);
 
     for(auto val : mySet){
-        std::cout<<val;
+        std::cout<<val<<'\n';
     }
+
+    std::set<float> initialiseSet = {20.0, 10.5, 132.7};
+    // does NOT print in order! Because it maintians elements in ascending order by default
+    for(auto &val : initialiseSet){
+        // val = 10.3; THIS WONT WORK BECAUSE IT's READ ONLY
+        std::cout<<&val<<' ';
+        std::cout<<val<<'\n';
+    }
+
+
+   
+
 }
