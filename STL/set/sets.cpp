680577372bde08cbce8b859c25c46b8be77fba1f
syedshazli <syed.abdu1818@gmail.com>
2025-05-26 17:06:16 -0700

    finish sets, begin string search

--- STL/set/sets.cpp
+++ STL/set/sets.cpp
@@ -5,6 +5,7 @@ int main(){
     // in sets, the value IS the key.. so no keys can be the same and no vals can be the same
     // ONLY UNIQUE VALUES
     // cant be modified once accessed
+    // but items can be inserted or removed after init
     // NOT TO BE CONFUSED WITH A HASHMAP!!! NO <INT, INT> KIND OF RELATIONSHIP
     std::set<int> mySet;
     mySet.emplace(10);
@@ -21,6 +22,7 @@ int main(){
         std::cout<<val<<'\n';
     }
 
+    
 
    
 
