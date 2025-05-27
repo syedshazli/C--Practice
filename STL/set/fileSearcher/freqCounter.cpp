f4f1aa4fb27d88041d9f2bb36f1adb15ca0673c1
syedshazli <syed.abdu1818@gmail.com>
2025-05-26 17:13:05 -0700

    init freqCounter

---
+++ STL/set/fileSearcher/freqCounter.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <set>
+#include <iostream>
+#include <fstream>
+// Word Frequency Counter: 
+// Read a text file and use a set<string> to store unique words, then count how many unique words exist
+int main(){
+
+}
