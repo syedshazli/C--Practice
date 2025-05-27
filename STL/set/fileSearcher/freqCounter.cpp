700782d699f6e2135779324e3795b919c766b9d5
syedshazli <syed.abdu1818@gmail.com>
2025-05-26 17:23:45 -0700

    finish freqency counter

--- STL/set/fileSearcher/freqCounter.cpp
+++ STL/set/fileSearcher/freqCounter.cpp
@@ -1,9 +1,18 @@
-#include <string>
 #include <set>
 #include <iostream>
 #include <fstream>
+#include <string>
 // Word Frequency Counter: 
 // Read a text file and use a set<string> to store unique words, then count how many unique words exist
 int main(){
+    std::ifstream input("file.txt");
+    std::set<std::string> parsingSet;
+
+    std::string word;
+    while (input >> word){
+        parsingSet.emplace(word);
+    }
+
+    std::cout<<"The text has "<<parsingSet.size()<<" unique words "<<'\n';
 
 }
