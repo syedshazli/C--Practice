1894c3f58797868f49d22c3a2650b1304a53e56e
syedshazli <syed.abdu1818@gmail.com>
2025-05-29 22:01:15 -0700

    finish barrier

--- Barriers/barrier.cpp
+++ Barriers/barrier.cpp
@@ -1,8 +1,18 @@
 #include <thread>
 #include <iostream>
+#include <atomic>
 
+const int numThreads = 8;
+std::atomic_int threadsWaiting = 0;
 
-void barrier(){
+void barrier(std::thread myTHread){
+    ++threadsWaiting; // pre increment threads waiting
+    while(true){
+        if(threadsWaiting == numThreads){
+            break;
+        }
+        else{continue;}
+    }    
 
 }
 
