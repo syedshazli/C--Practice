76da9a9cbf5df72cc125c957af377a97c4f60b49
syedshazli <syed.abdu1818@gmail.com>
2025-06-01 22:04:32 -0700

    finish exception

---
+++ exceptions/exceptionCalled.cu
@@ -0,0 +1,18 @@
+
+__global__ callMe(int *dev_a){
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    // exception thrown here.
+    dev_a[tid+10000] = tid;
+}
+
+
+int main(){
+
+    int *dev_a;
+    int host_a[4];
+    cudaMalloc( (void**) &dev_a, sizeof(int)*4);
+  
+    callMe<<<1, 16>>> (dev_a);
+
+    cudaMemcpy(host_a, dev_a, 4*sizeof(int), cudaMemcpyDeviceToHost);
+}
