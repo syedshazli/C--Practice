18ca4690ebe26023578453411e3c944d52565111
syedshazli <syed.abdu1818@gmail.com>
2025-06-08 22:00:22 -0700

    finish inefficent

---
+++ caseSwitchAlternative/inefficent.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main(){
+
+    int i = 0;
+    switch (i){
+        case(0):
+            std::cout<<0;
+        
+    
+        case(1):
+            std::cout<<1;
+        
+        case(2):
+            std::cout<<2;
+
+        case(3):
+            std::cout<<3;
+
+
+        case(4):
+            std::cout<<4;
+        
+        case(5):
+            std::cout<<5;
+
+        }
+}
