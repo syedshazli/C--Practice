7eec1987ceb5c80610c400e98c7e5520a55645c9
syedshazli <syed.abdu1818@gmail.com>
2026-03-26 10:01:56 -0400

    init

---
+++ From_Scratch/Vector/std_vec.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+class Vector{
+
+    // takes a templated array and adds values to the end of it
+    void push_back(& array){
+
+    }
+};
+
+int main()
+{
+    // implementation should allow this to be initilaized.
+    Vector<int,5> tryMe = {1,2,3,4,5};
+}
