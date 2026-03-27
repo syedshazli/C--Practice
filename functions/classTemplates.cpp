25c2b1d53905b540edeeb697c47d359e0499332c
syedshazli <syed.abdu1818@gmail.com>
2026-03-27 12:43:55 -0400

    error

--- functions/classTemplates.cpp
+++ functions/classTemplates.cpp
@@ -2,9 +2,20 @@
 #include <array>
 #include <vector>
 
+template <typename T>
+class helloThere{
 
+    void speak(T speaker)
+    {
+        auto name = speaker;
+        std::cout<<"Hey there "<<name<<" ";
+    }
+
+};
 
 int main()
 {
+    auto helloT = new helloThere;
+    helloT.speak("Skippy");
 
 }
