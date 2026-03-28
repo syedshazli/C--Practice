ed2b0fa401a5c46b56cebcfa2760b1cabaf5f1d9
syedshazli <syed.abdu1818@gmail.com>
2026-03-28 19:12:02 -0400

    init

--- From_Scratch/Vector/std_vec.cpp
+++ From_Scratch/Vector/std_vec.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 
+// different ways to initialize a std::vector, must be implemented here.
+// example: can init with vector<int, 5>, with no predefined size, etc.
 class Vector{
 
     // takes a templated array and adds values to the end of it
+    // aka we must allocate memory for a new element, resizing the original array
     void push_back(& array){
-
+        int sizeArray = sizeof(array);
+        
+        auto *push_back_array = malloc(sizeArray+1);
+        fill_new_array
+        delete(array);
     }
 };
 
