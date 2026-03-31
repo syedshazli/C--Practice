53d41b4cd020d17307afe26c8b7481b4602b58b5
syedshazli <syed.abdu1818@gmail.com>
2026-03-31 18:25:54 -0400

    init pop_back

--- From_Scratch/Vector/std_vec.cpp
+++ From_Scratch/Vector/std_vec.cpp
@@ -6,13 +6,19 @@ class Vector{
 
     // takes a templated array and adds values to the end of it
     // aka we must allocate memory for a new element, resizing the original array
-    void push_back(& array){
+    void push_back(T dtype){
         int sizeArray = sizeof(array);
         
-        auto *push_back_array = malloc(sizeArray+1);
-        fill_new_array
+        auto *push_back_array = malloc(dtype); // allocate another n bytes for  the next element
+        fill_new_array()
         delete(array);
     }
+
+    void pop_back(int index)
+    {
+        // free element at index 'index' of the array so that it is no longer part of the vector
+    }
+
 };
 
 int main()
