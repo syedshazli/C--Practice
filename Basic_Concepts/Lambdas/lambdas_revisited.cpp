23ee9a7709998c0e58ab840dc409fc152c285274
syedshazli <syed.abdu1818@gmail.com>
2026-05-13 23:49:36 -0400

    done

--- Basic_Concepts/Lambdas/lambdas_revisited.cpp
+++ Basic_Concepts/Lambdas/lambdas_revisited.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
-    
+    float grade = 4.0;
+    auto printHello = [&](int age){ // use & to allow pass by ref for any variables that exist currently before the func. Can also do &grade
+        grade = 2.9;
+        std::cout<<"This is the message, where you are "<<age<<" years old"<<std::endl;
+    };
+
+    printHello(10);
+    std::cout<<"After using capture, grade is now "<<grade;
 }
