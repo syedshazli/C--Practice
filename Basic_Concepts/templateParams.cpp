928f1914f722b1793aa5b5ade3fa2ea844b4a212
syedshazli <syed.abdu1818@gmail.com>
2026-05-22 16:45:54 -0400

    done

--- Basic_Concepts/templateParams.cpp
+++ Basic_Concepts/templateParams.cpp
@@ -7,13 +7,22 @@
 
 // When the function wasnt a part of any class that would contain those arguments
 
+// if we want this to be passed in, we must specify the type here
 template<int val>
 void printStatus(int param)
 {
 std::cout<<"The parameter valie is "<<param<<" while the template value is " <<val <<std::endl;
 }
 
+// can still have templated values as well as multiple params passed, but not floats
+template<typename dType, int val, int secondVal>
+void printTwo(dType dataType)
+{
+    std::cout<<"The templated value passed in was "<<dataType<<" while the first integer passed in thru templated parameters was "<<val<<" while the second was " <<secondVal<<std::endl;
+}
+
 int main()
 {
 printStatus<3>(2);
+printTwo<int, 3, 5>(7);
 }
